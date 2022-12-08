842d96027b58f7fc1a37103d2f44b40933ac144d
Dustin Zeisler <dustin@zeisler.net>
2022-12-07 22:41:35 -0800

    Delete ignored files

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-./completions.sh
-
-if ! command -v goreleaser &> /dev/null; then
-   GOWORK=off curl -sfL https://goreleaser.com/static/run | DISTRIBUTION=pro bash -s -- release --rm-dist --skip-sign  || exit 1
-else
-  GOWORK=off goreleaser release --rm-dist --skip-sign
-fi
