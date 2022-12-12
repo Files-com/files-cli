196b317bd044315e10723cbd619f6145d30531d9
Dustin Zeisler <dustin.zeisler@files.com>
2022-12-12 14:59:06 -0800

    Remove ignored files

--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if ! command -v goreleaser &> /dev/null; then
-  curl -sfL https://goreleaser.com/static/run | GOWORK=off DISTRIBUTION=pro bash -s -- release --rm-dist --skip-sign || exit 1
-else
-  GOWORK=off goreleaser release --rm-dist --skip-sign
-fi
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "${DIR}" || exit 1
-go test ./... -buildvcs=false
-ERROR_CODE=$?
-
-exit ${ERROR_CODE}
